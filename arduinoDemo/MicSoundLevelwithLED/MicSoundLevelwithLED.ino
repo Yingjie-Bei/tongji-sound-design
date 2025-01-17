d680453b462cb5a8728fda7bf280ef9d121a3437
YJ <brady_bei@outlook.com>
2025-01-17 15:10:37 -0500

    added arduinoDemo

---
+++ arduinoDemo/MicSoundLevelwithLED/MicSoundLevelwithLED.ino
@@ -0,0 +1,70 @@
+const int sampleWindow = 50;  // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
+#endif
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN 6
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 32
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+void setup() {
+  Serial.begin(9600);
+
+  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();             // Turn OFF all pixels ASAP
+  strip.setBrightness(50);  // Set BRIGHTNESS to about 1/5 (max = 255)
+}
+
+
+void loop() {
+  unsigned long startMillis = millis();  // Start of sample window
+  unsigned int peakToPeak = 0;           // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 1024;
+
+  // collect data for 50 mS
+  while (millis() - startMillis < sampleWindow) {
+    sample = analogRead(0);
+    if (sample < 1024)  // toss out spurious readings
+    {
+      if (sample > signalMax) {
+        signalMax = sample;  // save just the max levels
+      } else if (sample < signalMin) {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+  }
+  peakToPeak = signalMax - signalMin;        // max - min = peak-peak amplitude
+  double volts = (peakToPeak * 5.0) / 1024;  // convert to volts
+
+  Serial.println(volts);
+
+  int color = 0;
+  if (volts > 1) {
+    color = 255;
+  }
+
+  for (int i = 0; i < strip.numPixels(); i++) {   // For each pixel in strip...
+    strip.setPixelColor(i, color);  //  Set pixel's color (in RAM)
+    strip.show();                                 //  Update strip to match
+    delay(1);                                     //  Pause for a moment
+  }
+}
