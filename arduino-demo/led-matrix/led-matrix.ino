61ed6905626664a89bbf611dfe243d280fd19407
YJ <brady_bei@outlook.com>
2025-01-17 16:55:46 -0500

    added breadboard png and new file naming

--- arduino-demo/led-matrix/led-matrix.ino
+++ arduino-demo/led-matrix/led-matrix.ino
@@ -6,7 +6,7 @@
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
 #ifndef PSTR
- #define PSTR // Make Arduino Due happy
+#define PSTR  // Make Arduino Due happy
 #endif
 
 #define PIN 6
@@ -54,12 +54,12 @@
 // or column order.  The matrices use 800 KHz (v2) pixels that expect GRB
 // color data.
 Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(8, 8, 4, 1, PIN,
-  NEO_TILE_TOP   + NEO_TILE_RIGHT   + NEO_TILE_ROWS   + NEO_TILE_PROGRESSIVE +
-  NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
-  NEO_GRB + NEO_KHZ800);
+                                               NEO_TILE_TOP + NEO_TILE_RIGHT + NEO_TILE_ROWS + NEO_TILE_PROGRESSIVE + NEO_MATRIX_BOTTOM + NEO_MATRIX_RIGHT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+                                               NEO_GRB + NEO_KHZ800);
 
 const uint16_t colors[] = {
-  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255)
+};
 
 void setup() {
   matrix.begin();
@@ -68,16 +68,16 @@ void setup() {
   matrix.setTextColor(colors[0]);
 }
 
-int x    = matrix.width();
+int x = matrix.width();
 int pass = 0;
 
 void loop() {
   matrix.fillScreen(0);
   matrix.setCursor(x, 0);
   matrix.print(F("TJ D&I"));
-  if(--x < -36) {
+  if (--x < -36) {
     x = matrix.width();
-    if(++pass >= 3) pass = 0;
+    if (++pass >= 3) pass = 0;
     matrix.setTextColor(colors[pass]);
   }
   matrix.show();
